d0e59fe6745b8660c9ed89151ea34f365424d845
Hikaru Sato <>
2020-06-06 23:54:25 +0900

    動くように完成

--- app/startup.sh
+++ app/startup.sh
@@ -3,4 +3,4 @@
 dotnet /web/Implem.CodeDefiner/Implem.CodeDefiner.NetCore.dll _rds
 
 cd /web/Implem.Pleasanter
-dotnet Implem.Pleasanter.NetCore.dll
+dotnet Implem.Pleasanter.NetCore.dll --urls=http://0.0.0.0:5000
